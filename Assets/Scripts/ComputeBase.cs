ea43cb43a9dfb2f6b9f9ef802a24788fdbec173c
Codrin-Mihail <kodrintxps@gmail.com>
2020-05-29 18:38:49 +0900

    basic abstract class

---
+++ Assets/Scripts/ComputeBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FlowField
+{
+    
+    [System.Serializable]
+    public enum ThreadCount
+    {
+        T_1 = 1,
+        T_4 = 4,
+        T_8 = 8,
+        T_16 = 16,
+        T_32 = 32,
+        T_64 = 64,
+        T_128 = 128,
+        T_256 = 256,
+        T_512 = 512,
+        T_1024 = 1024
+    }
+    
+    
+    public abstract class ComputeBase : MonoBehaviour
+    {
+
+        [Header("SIMULATION PARAMS")] 
+        [SerializeField] protected Vector3 simulationSpace = Vector3.one;
+
+        [SerializeField] protected ThreadCount TCOUNT_X = ThreadCount.T_256;
+        [SerializeField] protected ThreadCount TCOUNT_Y = ThreadCount.T_1;
+        [SerializeField] protected ThreadCount TCOUNT_Z = ThreadCount.T_1;
+
+        protected void Awake()
+        {
+            
+        }
+
+        protected virtual void Start()
+        {
+            
+        }
+
+        protected virtual void Update()
+        {
+            
+        }
+
+        protected abstract void ComputeInit();
+        protected abstract void GenerateBuffers();
+        protected abstract void SetConstants();
+        protected abstract void Dispatch();
+
+        protected void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireCube(this.transform.position, simulationSpace);
+        }
+    }
+}
